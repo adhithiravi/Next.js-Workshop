0ab9b82301ba5a36461973739d4438a108a0f62c
adhithiravi <adhithiravi@gmail.com>
2025-08-08 11:43:20 -0500

    Add pie card skeleton to loading file

--- app/shop/loading.tsx
+++ app/shop/loading.tsx
@@ -1,8 +1,18 @@
+import Navigation from "../components/Navigation";
+import PieCardSkeleton from "../components/PieCardSkeleton";
+
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
-      <p className="mt-4 text-gray-600">Loading...</p>
-    </div>
+    <>
+      <Navigation />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <PieCardSkeleton />
+        <PieCardSkeleton />
+        <PieCardSkeleton />
+        <PieCardSkeleton />
+        <PieCardSkeleton />
+        <PieCardSkeleton />
+      </div>
+    </>
   );
 }
